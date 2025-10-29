22181239738944d426e090af24799e4474bd2adb
Junu0201 <yeoj@wsc.school.nz>
2025-10-30 10:13:15 +1300

    Done

--- settings_menu.gd
+++ settings_menu.gd
@@ -3,7 +3,21 @@ extends Control
 
 
 func _on_volume_value_changed(value):
-	AudioServer.set_bus_volume_db(0,value)
+	AudioServer.set_bus_volume_db(0,value/5)
 
 func _on_check_box_toggled(toggled_on):
-	pass # Replace with function body.
+	AudioServer.set_bus_mute(0,toggled_on)
+
+
+func _on_back_pressed():
+	get_tree().change_scene_to_file("res://Scenes/options_menu.tscn")
+
+
+func _on_resolutions_item_selected(index):
+	match index:
+		0:
+			DisplayServer.window_set_size(Vector2i(1920,1080))
+		1:
+			DisplayServer.window_set_size(Vector2i(1600,900))
+		2:
+			DisplayServer.window_set_size(Vector2i(1280,720))
